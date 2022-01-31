31a5eb817d7544bae61e9e8011d72bbedba65634
Your Name <you@example.com>
2022-01-31 13:38:45 -0500

    Project 1

---
+++ Discord-Bot/README.md
@@ -0,0 +1,12 @@
+SETUP
+- First you have to create a Discord Bot and a guild. Then you will have go to Developer Portal and Applications. Then go to Bot tab. Then you will see a colum that says Token and that where you will find the token.
+- You would pu the Token in a .env file along with the guild name.
+- Packages that need to be installed were: discord.py, dotenv, and python (if not installed). All were downloaded using pip.
+
+Usage:
+- I have created a bot client and used the prefix ! in order to run the command. Then I created a python code saying the content has the right requiremnt then send this command. 
+- I can type !test and the command will result in the output
+- A message saying Working! and a image of a cat.
+
+Reseach:
+- Maybe we can add a sleep time so it will be running aslong as we want it to run. This would work becuase this controls how long you want he program to run and you dont have to keep on pressing or typing to run it. We can put the sleep time for 1 day or 1 year and it will continously run no matter what. 
--- Discord-Bot/bot.py
+++ Discord-Bot/bot.py
@@ -5,14 +5,8 @@ import discord
 import random
 from discord.ext import commands
 from dotenv import load_dotenv
-<<<<<<< HEAD
 from discord.ext.commands import bot
 from discord import File
-=======
-from discord.ext import commands
-from discord.ext.commands import bot
->>>>>>> cdb7493815c3783473e2f29426533154e7a59fd1
-
 
 load_dotenv()
 #print(os.getenv('DISCORD_TOKEN'))
@@ -20,10 +14,8 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
 bot = commands.Bot(command_prefix='!')
+
 client = discord.Client()
-@bot.event
-async def on_ready():
-    print('Connected')
 
 @bot.event
 async def on_ready():
@@ -42,13 +34,8 @@ async def on_ready():
 
 @bot.command(pass_context=True)
 async def test(ctx):
-<<<<<<< HEAD
     await ctx.send('Working!', file=discord.File("cat.png"))
 
-=======
-    await ctx.send('Working!', file=discord.File("\Downloads\cat.png"))
-        
->>>>>>> cdb7493815c3783473e2f29426533154e7a59fd1
 @client.event
 async def on_message(message):
         if message.author == client.user:
@@ -76,10 +63,6 @@ async def on_message(message):
             response = random.choice(hitchhiker_quotes)
             await message.channel.send(response)
 
-
-
-
-
 bot.run(TOKEN)
 client.run(TOKEN)
-bot.run(TOKEN)
+
